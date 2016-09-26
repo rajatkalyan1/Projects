100bc1c114cbd24dc41ddbeb72e2c8accbf306e0
rajatkalyan1 <rajatkalyan@gmail.com>
2016-09-26 09:38:27 -0500

    Add files via upload

---
+++ Entertainment-Center-Website/entertainment_center.py
@@ -0,0 +1,12 @@
+import media
+import fresh_tomatoes
+
+toy_story = media.Movie("Toy Story",
+                        "A story of a boy and his toys that come to life",
+                        "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+                        "https://www.youtube.com/watch?v=vwyZH85NQC4")
+avatar = media.Movie("Avatar", "A marine on an alien planet",
+                     "http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",
+                     "http://www.youtube.com/watch?v=-9ceBgWV8io")
+movies = [toy_story, avatar]
+fresh_tomatoes.open_movies_page(movies)
